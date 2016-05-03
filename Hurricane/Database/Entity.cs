1d4d063ade074a2dc6db01a9685f9c9df0dcf6f3
vain0 <id_vain@yahoo.co.jp>
2016-05-03 13:07:08 +0900

    Add database

---
+++ Hurricane/Database/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hurricane.Database
+{
+    public static class Entity
+    {
+        private static HurricaneEntities _instance;
+        public static HurricaneEntities Instance
+        {
+            get { return _instance ?? (_instance = new HurricaneEntities()); }
+        }
+    }
+}
